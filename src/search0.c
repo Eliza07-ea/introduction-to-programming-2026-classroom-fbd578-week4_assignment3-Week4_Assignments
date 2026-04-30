864927376e923f5e2b20f5081adf9b37753dec50
Eliza07-ea <elizaabdul7@gmail.com>
2026-04-30 20:48:54 +0200

    Update search0.c

--- src/search0.c
+++ src/search0.c
@@ -1,19 +1,21 @@
-/*
-WEEK 1 — TASK 3 (Linear Search - Integers)
+#include <cs50.h>
+#include <stdio.h>
 
-Goal:
-Practice arrays and linear search.
+int main(void)
+{
+    int numbers[] = {4, 8, 15, 16, 23, 42, 99, 3, 7, 21};
+    int size = 10;
 
-Task:
-1. Create an array of integers.
-2. Ask the user for a number.
-3. Search the array manually (no library search).
-4. If found print:
-Found
-   Otherwise print:
-Not found
+    int query = get_int("Number: ");
 
-Rules:
-- You must use a loop.
-- Do NOT use any built-in search function.
-*/
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] == query)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
+}
